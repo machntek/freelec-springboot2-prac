3b1f96b1c5ffeb2df10301e191db4f5ef56ea686
machntek <machntek@gmail.com>
2021-01-28 22:28:57 +0900

    스프링부트 study(세션객체 어노테이션 resolve, 세션 db저장 까지)

---
+++ src/main/java/com/machntek/prac/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.machntek.prac.springboot.web.dto;
+
+import com.machntek.prac.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
